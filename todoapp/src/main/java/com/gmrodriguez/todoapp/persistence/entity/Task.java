d6438b6147f90bdb25ec16314ecc816250be0b71
Gonzalo Rodriguez <gmrodriguez12@gmail.com>
2023-02-25 12:35:57 -0300

    task entity

---
+++ todoapp/src/main/java/com/gmrodriguez/todoapp/persistence/entity/Task.java
@@ -0,0 +1,21 @@
+package com.gmrodriguez.todoapp.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdDate;
+    private LocalDateTime eta;
+    private boolean finished;
+    private TaskStatus status;
+}
---
+++ todoapp/src/main/java/com/gmrodriguez/todoapp/persistence/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.gmrodriguez.todoapp.persistence.entity;
+
+public enum TaskStatus {
+    ON_TIME,
+    LATE,
+}
